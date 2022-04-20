6f00cb59dde00fb0c965bd4e83a954108d2a8c5b
mohammadlebzo <mohlebzo1@gmail.com>
2022-04-20 20:09:52 +0300

    Finished task 06

--- js/app.js
+++ js/app.js
@@ -1,55 +1,88 @@
 let genderAccept = false;
 let ageAccept = false;
 let welAccept = false;
+let genderAnsr = '';
 
 let fname = prompt('Please enter your name:');
-let gender = prompt('Please enter your gender: (male/female)');
-let age = 0;
 
-while (genderAccept === false) {
-    if (gender.toLowerCase() === 'male' || gender.toLowerCase() === 'female') {
-        genderAccept = true;
-    }
-    else{
-        alert('input not accepted, try again');
-        gender = prompt('Please enter your gender: (male/female)');
-        
-    }
-}
-
-if (genderAccept) {
-    age = prompt('Please enter your age:');
-    while(ageAccept === false){
-        if (age > 0) {
-            ageAccept = true;
+const genderQ = gender => {
+    while (genderAccept === false) {
+        if (gender.toLowerCase() === 'male' || gender.toLowerCase() === 'female') {
+            genderAccept = true;
+            genderAnsr = gender;
         }
         else{
             alert('input not accepted, try again');
-            age = 0;
-            age = prompt('Please enter your age:');
+            gender = prompt('Please enter your gender: (male/female)');
+            
         }
     }
 }
 
-if (ageAccept) {
-    let welMessage = prompt('Do you want to skip the welcoming message ? (yes/no)');
-    while (welAccept === false) {
-        if (welMessage.toLowerCase() === 'yes') {
-            welAccept = true;
-            alert('OK!');
-        }
-        else if (welMessage.toLowerCase() === 'no') {
-            if (gender.toLowerCase() === 'male') {
-                alert(`Hellow Mr ${fname}`);
+const ageQ = age => {
+    if (genderAccept) {
+        age = prompt('Please enter your age:');
+        while(ageAccept === false){
+            if (age > 0) {
+                ageAccept = true;
             }
             else{
-                alert(`Hellow Ms ${fname}`);
+                alert('input not accepted, try again');
+                age = 0;
+                age = prompt('Please enter your age:');
             }
-            welAccept = true;
-        }
-        else{
-            alert('input not accepted, try again');
-            welMessage = prompt('Do you want to skip the welcoming message ? (yes/no)');
         }
     }
 }
+
+const welMessageQ = () => {
+    if (ageAccept) {
+        let welMessage = prompt('Do you want to skip the welcoming message ? (yes/no)');
+        while (welAccept === false) {
+            if (welMessage.toLowerCase() === 'yes') {
+                welAccept = true;
+                alert('OK!');
+            }
+            else if (welMessage.toLowerCase() === 'no') {
+                if (genderAnsr.toLowerCase() === 'male') {
+                    alert(`Hellow Mr ${fname}`);
+                }
+                else{
+                    alert(`Hellow Ms ${fname}`);
+                }
+                welAccept = true;
+            }
+            else{
+                alert('input not accepted, try again');
+                welMessage = prompt('Do you want to skip the welcoming message ? (yes/no)');
+            }
+        }
+    }
+}
+
+const threeQs = () => {
+    let arr = [];
+    alert('Answer the upcoming questions with YES or NO.');
+    let q1 = prompt('Are you a programmer ?');
+    qTest(q1, arr);
+    let q2 = prompt('Do you like pets?');
+    qTest(q2, arr);
+    let q3 = prompt('Do you have a pet ?');
+    qTest(q3, arr);
+    for (let i = 0; i < arr.length; i++) 
+        console.log(arr[i]);
+}
+
+const qTest = (question, arr) => {
+    if (question.toLowerCase() === 'yes' || question.toLowerCase() === 'no') {
+        arr.push(question.toLowerCase());
+    }
+    else{
+        arr.push('invalid');
+    }
+}
+
+genderQ(prompt('Please enter your gender: (male/female)'));
+ageQ(0);
+welMessageQ();
+threeQs();
